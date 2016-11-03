225336456776813cf9da9dee075d040e9f207c56
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-11-03 10:25:51 -0400

    Create WeatherStation and WeatherReading models and add seed tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021000002) do
+ActiveRecord::Schema.define(version: 20160518144053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "floorplans", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "rent"
-    t.integer  "bathroom_count"
-    t.integer  "bedroom_count"
-    t.integer  "property_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["property_id"], name: "index_floorplans_on_property_id", using: :btree
+  create_table "weather_readings", force: :cascade do |t|
+    t.string   "station"
+    t.date     "reading_date"
+    t.string   "reading_type"
+    t.integer  "reading_value"
+    t.string   "measurement_flag"
+    t.string   "quality_flag"
+    t.string   "source_flag"
+    t.integer  "observation_time"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["reading_date", "reading_type"], name: "index_weather_readings_on_reading_date_and_reading_type", using: :btree
   end
 
-  create_table "managements", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "properties", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "formatted_address"
-    t.string   "address"
-    t.string   "city"
-    t.string   "county"
-    t.string   "state"
-    t.string   "state_code"
-    t.string   "zip"
-    t.string   "country"
+  create_table "weather_stations", force: :cascade do |t|
+    t.string   "station_id"
     t.float    "latitude"
     t.float    "longitude"
-    t.json     "amenities"
-    t.json     "pet"
-    t.integer  "rent_min"
-    t.integer  "rent_max"
-    t.integer  "management_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.index ["management_id"], name: "index_properties_on_management_id", using: :btree
+    t.float    "elevation"
+    t.string   "state"
+    t.string   "name"
+    t.string   "gsn_flag"
+    t.string   "hcn_flag"
+    t.string   "wmo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["station_id"], name: "index_weather_stations_on_station_id", using: :btree
   end
 
-  add_foreign_key "floorplans", "properties"
-  add_foreign_key "properties", "managements"
 end
